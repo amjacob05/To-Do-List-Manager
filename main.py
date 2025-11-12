6c49cc1cfb74a443e257ba924b59f0d1b7945a41
amjacob05 <ankit.m.jacob@gmail.com>
2025-11-12 00:17:04 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -2,5 +2,67 @@
 # MSE121 Project: To-Do List Manager
 # Purpose: Manage a simple to-do list with add/remove/mark-done check
 
+def load_tasks():
+    """
+    Reads the tasks from a text file and returns a list of task as dictionaries.
+    Each line will be in the format: task_name|done_flag
+    Example: "Buy milk|False"
+    If the file does not exist, return an empty list.
+    """
+    pass  
+
+
+def save_tasks():
+    """
+    Saves the list of task dictionaries back to the text file.
+    Overwrites to the file each time.
+    """
+    pass
+
+
+def add_task():
+    """
+    Adds a new task dictionary to the tasks list.
+    """
+    pass
+
+
+def remove_task():
+    """
+    Remove a task at the given index.
+    If index invalid, print an error message.
+    """
+    pass
+
+
+def mark_done():
+    """
+    Mark a task as completed.
+    If already done or invalid index, handle code
+    """
+    pass
+
+
+def list_tasks():
+    """
+    Return a formatted string showing each task with its status.
+    Example output:
+    1. [ ] Buy milk
+    2. [x] Finish homework
+    """
+    pass
+
+
+def menu_loop():
+    """
+    The main user interface loop.
+    Show menu options, call other functions, save before exit.
+    """
+    pass
+
+if __name__ == "__main__":
+    
+    pass
+
 
 
